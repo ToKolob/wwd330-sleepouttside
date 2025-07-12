13a90e686364093a21340b26a9cdf0881ae71f08
Sree Ranganath <skoonisetti@byupathway.edu>
2025-07-12 20:12:02 +0530

    sree--W02individualassignment

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -15,18 +15,28 @@ export default class ProductDetails {
       .addEventListener('click', this.addToCart.bind(this));
   }
 
-  addToCart() {
-    let cartContents = getLocalStorage('so-cart') || [];
+  addProductToCart() {
+    const cartContents = getLocalStorage('so-cart') || [];
     cartContents.push(this.product);
     setLocalStorage('so-cart', cartContents);
   }
 
   renderProductDetails() {
-    document.getElementById('product-name').textContent = this.product.Name;
-    document.title = this.product.Name + ' | Sleep Outside';
-    document.getElementById('product-image').src = this.product.Image;
-    document.getElementById('product-image').alt = this.product.Name;
-    document.getElementById('product-price').textContent = `$${this.product.FinalPrice}`;
-    document.getElementById('product-description').innerHTML = this.product.Description;
+    productDetailsTemplate(this.product);
   }
+}
+
+    function productDetailsTemplate(product) {
+  document.querySelector("h2").textContent = product.Brand.Name;
+  document.querySelector("h3").textContent = product.NameWithoutBrand;
+
+  const productImage = document.getElementById("productImage");
+  productImage.src = product.Image;
+  productImage.alt = product.NameWithoutBrand;
+
+  document.getElementById("productPrice").textContent = product.FinalPrice;
+  document.getElementById("productColor").textContent = product.Colors[0].ColorName;
+  document.getElementById("productDesc").innerHTML = product.DescriptionHtmlSimple;
+
+  document.getElementById("addToCart").dataset.id = product.Id;
 }
